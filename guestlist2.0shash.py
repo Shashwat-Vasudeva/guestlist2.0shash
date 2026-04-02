9258d80c32bce70052b75b012e1e006d209d0247
Shashwat-Vasudeva <shashwatv004@deltalearns.ca>
2026-04-01 21:05:55 -0700

    formating names

---
+++ guestlist2.0shash.py
@@ -0,0 +1,12 @@
+# create empty list called guests 
+
+#Function format_name(name)
+#remove extra space
+
+#function is_duplicate(name)
+# if name in guests
+#return ture
+#else 
+#return false
+
+#formating functions---
